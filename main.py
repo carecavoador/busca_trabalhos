f45112ee42848e95069e68237d78b31d7382b952
Careca Voador <e.rodrigo@outlook.com>
2022-10-24 18:16:34 -0300

    Refatorando tudo.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """main.py"""
 import sys
-import os
+import re
 import time
 
+from io import BytesIO
 from shutil import copy
 from pathlib import Path
 from datetime import datetime
@@ -10,16 +11,18 @@ from zipfile import ZipFile, BadZipFile
 
 
 from configuracoes import console, carrega_config
-from trabalhos import Trabalho
+from trabalhos import Trabalho, ItensExpedir
 
 
 HOJE = datetime.today().strftime("%d-%m-%Y")
-AGORA = datetime.now().strftime("%H-%M-%S")
+AGORA = datetime.now().strftime("%H_%M_%S")
 
-RE_NUM_OS = r"(\d{4,}).*[vV](\d+)"
+# RE_NUM_OS = r"(\d{4,}).*[vV](\d+)"
+RE_NUM_OS = r"(?P<os>\d{4,}).+[vV](?P<versao>\d+)"
 
 
-def descompacta(_arquivo: Path, _novo_arquivo: Path) -> None:
+def descompacta(_arquivo: Path) -> list[tuple[str, BytesIO]]:
+    conteudo = []
     try:
         with ZipFile(_arquivo, mode="r") as arquivo_zip:
             _lista_arquivos = arquivo_zip.infolist()
@@ -28,82 +31,82 @@ def descompacta(_arquivo: Path, _novo_arquivo: Path) -> None:
                 # Ignora item se for diretório.
                 if item.is_dir():
                     continue
-
                 # Extrai apenas o nome de arquivo, ignorando subpastas.
                 item.filename = item.filename.split("/")[-1]
-
                 # Ignora itens na lista de ignorar.
                 if item.filename in ["Thumbs.db"]:
                     continue
-
                 # Ignora itens ocultos (que começam com ".").
                 if item.filename.startswith("."):
                     continue
-
-                if len(_lista_arquivos) > 1:
-                    item.filename = (
-                        _novo_arquivo.stem
-                        + "_"
-                        + str(idx)
-                        + "."
-                        + item.filename.split(".")[-1]
-                    )
-                    idx += 1
-                else:
-                    item.filename = (
-                        _novo_arquivo.stem + "." + item.filename.split(".")[-1]
-                    )
-
-                arquivo_zip.extract(item, path=_novo_arquivo.parent)
-    except BadZipFile as erro:
-        print(f"Não foi possível abrir o arquivo {_arquivo.name}.")
-        print(erro)
+                conteudo.append((item.filename, BytesIO(arquivo_zip.read(item))))
+    except BadZipFile:
+        raise BadZipFile
+    return conteudo
 
 
-def busca_arquivos(
-    job: Trabalho, entrada: Path, dir_saida: Path, tipo: str, sufixo: str = ""
-) -> None:
-    """Tenta localizar arquivos relativos ao Job no local especificado, depois
-    tenta baixar os respectivos arquivos do FTP."""
+def adivinha_num_os(nome_arquivo: str) -> tuple[int, int]:
+    padrao = re.compile(RE_NUM_OS)
+    resultado = re.findall(padrao, nome_arquivo)
+    if resultado:
+        try:
+            num_os, versao = int(resultado[0][0]), int(resultado[0][1])
+            return num_os, versao
+        except ValueError:
+            return 0, 0
 
-    _arquivos = [
-        Path(entrada, a)
-        for a in os.listdir(entrada)
-        if (adivinha_num_os(a) == (job.num_os, job.versao))
-        and Path(entrada, a).is_file()
-    ]
-    if _arquivos:
 
-        _saida_arquivos = dir_saida.joinpath(tipo)
-        if not _saida_arquivos.exists():
-            os.mkdir(_saida_arquivos)
+def busca_arquivos(trabalho: Trabalho, dir_entrada: Path, dir_saida: Path, tipo_arquivo: ItensExpedir, sufixo: str = "") -> list[Path]:
+    """Tenta localizar arquivos relativos ao Trabalho no local especificado, depois tenta baixar os respectivos arquivos do FTP."""
+    arquivos_encontrados = [arquivo for arquivo in dir_entrada.iterdir() if adivinha_num_os(arquivo.name) == (trabalho.num_os, trabalho.versao)]
+    if not arquivos_encontrados:
+        return tipo_arquivo
+    dir_saida = dir_saida.joinpath(tipo_arquivo.value)
+    dir_baixados = dir_entrada.joinpath("Baixados")
+    dir_saida.mkdir(exist_ok=True)
+    dir_baixados.mkdir(exist_ok=True)
+    for arquivo in arquivos_encontrados:
+        # Adiciona _ (underline) à frente do sufixo
+        if sufixo:
+            sufixo = "_" + sufixo
+        # Caso seja arquivo .zip
+        if arquivo.suffix.lower() == ".zip":
+            try:
+                conteudo_zip = descompacta(arquivo)
+                for idx, item in enumerate(conteudo_zip):
+                    nome_arquivo, bytes_arquivo = item
+                    extensao = "." + nome_arquivo.split(".")[-1]
+                    num_arquivo = f"_{str(idx)}" if idx >= 1 else ""
+                    arquivo_saida = f"{trabalho.num_os}_p{trabalho.pedido}_v{trabalho.versao}_{tipo_arquivo.value}{sufixo}{num_arquivo}{extensao}"
+                    with open(dir_saida.joinpath(arquivo_saida), mode="wb") as arquivo_descompactado:
+                        arquivo_descompactado.write(bytes_arquivo.read())
+            except BadZipFile:
+                return tipo_arquivo
+            arquivo.unlink()
+            continue
+        # Caso não seja arquivo .zip
+        arquivo_saida = f"{trabalho.num_os}_p{trabalho.pedido}_v{trabalho.versao}_{tipo_arquivo.value}{sufixo}{arquivo.suffix}"
+        if dir_saida.joinpath(arquivo_saida).exists():
+            arquivo_saida = f"{trabalho.num_os}_p{trabalho.pedido}_v{trabalho.versao}_{tipo_arquivo.value}{sufixo}_{AGORA}{arquivo.suffix}"
+        copy(arquivo, dir_saida.joinpath(arquivo_saida))
+        if dir_baixados.joinpath(arquivo_saida).exists():
+            arquivo_saida = f"{trabalho.num_os}_p{trabalho.pedido}_v{trabalho.versao}_{tipo_arquivo.value}{sufixo}_{AGORA}{arquivo.suffix}"
+        copy(arquivo, dir_baixados.joinpath(arquivo_saida))
+        arquivo.unlink()
 
-        _baixados = entrada.joinpath("Baixados")
-        if not _baixados.exists():
-            os.mkdir(_baixados)
 
-        for _arquivo in _arquivos:
-            _novo_arquivo = _saida_arquivos.joinpath(
-                "".join([str(job), "_", tipo, sufixo, _arquivo.suffix])
-            )
-            if not _arquivo.suffix.lower() == ".zip":
-                copy(_arquivo, _novo_arquivo)
-            else:
-                descompacta(_arquivo, _novo_arquivo)
+def conta_ate(segundos: int, texto: str = "") -> None:
+    """Conta até cinco (segundos)."""
+    for i in range(segundos, 0, -1):
+        with console.status(f"{texto}{i}..."):
+            time.sleep(1)
 
-            _arquivo_baixado = _baixados.joinpath(_arquivo.name)
-            if not _arquivo_baixado.exists():
-                copy(_arquivo, _arquivo_baixado)
-                os.remove(_arquivo)
-            else:
-                _novo_arquivo_baixado = _baixados.joinpath(
-                    _arquivo.stem + "_" + AGORA + _arquivo.suffix
-                )
-                copy(_arquivo, _novo_arquivo_baixado)
-                os.remove(_arquivo)
-    else:
-        print(f"Não foi possível localizar arquivos de {tipo} para {job}.")
-        continua = input("Pressione qualquer tecla para continuar...\n> ")
+
+def encerrar() -> None:
+    """Encerra o programa."""
+    console.print("[green]Programa encerrado com sucesso!")
+    conta_ate(5, "Fechando em ")
+    sys.exit()
 
 
 def main() -> None:
@@ -120,38 +123,54 @@ def main() -> None:
     # lista_trabalhos = [
     #     Trabalho(Path(pdf)) for pdf in sys.argv if pdf.lower().endswith(".pdf")
     # ]
+    dir_ordens = Path(r"C:\Users\Everton Souza\Downloads")
     lista_trabalhos = [
-        Trabalho(pdf) for pdf in Path("testes/ordens").iterdir() if pdf.suffix == ".pdf"
+        Trabalho(pdf) for pdf in dir_ordens.iterdir() if pdf.suffix == ".pdf"
     ]
-
     if not lista_trabalhos:
-        input("Sem trabalhos no momento. Pressione 'Enter' para sair: ")
-        sys.exit()
-
+        console.print("Sem trabalhos no momento.")
+        encerrar()
+    arquivos_nao_encontrados = []
     for trabalho in lista_trabalhos:
-        console.rule(f"[bold green]{trabalho.resumo}", align="left")
+        erros = []
+        console.rule()
+        console.print(trabalho.resumo)
         console.print(trabalho.lista_materiais())
         with console.status("Buscando arquivos..."):
-            time.sleep(2)
-        # print(trabalho)
-        # if trabalho.precisa_layout:
-        #     busca_arquivos(trabalho, dir_layouts, dir_saida, "Layout")
-        # if trabalho.precisa_digital:
-        #     busca_arquivos(
-        #         trabalho,
-        #         dir_digitais,
-        #         dir_saida,
-        #         "Prova Digital",
-        #         sufixo=trabalho.perfil,
-        #     )
+            for item in trabalho.itens_expedir:
+                if item == ItensExpedir.LAYOUT:
+                    console.print(f"Buscando arquivo de {ItensExpedir.LAYOUT.value}...")
+                    falta_layouts = busca_arquivos(trabalho, dir_layouts, dir_saida, ItensExpedir.LAYOUT)
+                    if falta_layouts:
+                        erros.append(falta_layouts)
+                        console.print("[bold red]Não encontrado!")
+                    else:
+                        console.print("[bold green]OK!")
+                if item == ItensExpedir.DIGITAL:
+                    console.print(f"Buscando arquivo de {ItensExpedir.DIGITAL.value}...")
+                    falta_digitais = busca_arquivos(trabalho, dir_digitais, dir_saida, ItensExpedir.DIGITAL, sufixo=trabalho.perfil)
+                    if falta_digitais:
+                        erros.append(falta_digitais)
+                        console.print("[bold red]Não encontrado!")
+                    else:
+                        console.print("[bold green]OK!")
+        if erros:
+            arquivos_nao_encontrados.extend([{trabalho.resumo: erros}])
+    if arquivos_nao_encontrados:
+        console.rule("[red]ARQUIVOS NÃO ENCONTRADOS:", align="left")
+        for nao_encontrado in arquivos_nao_encontrados:
+            for k, v in nao_encontrado.items():
+                for material in v:
+                    console.print(f"[red]{k} -> {material.value}")
+        console.input("Pressione 'Enter' para fechar: ")
+        sys.exit()
+    encerrar()
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        console.input(
-            "[red]Programa cancelado pelo usuário. Pressione 'Enter' para fechar: "
-        )
+        console.print("[red]Programa cancelado pelo usuário.")
+        conta_ate(3)
         sys.exit()
-    console.input("[green]Programa terminado. Pressione 'Enter' para fechar: ")
